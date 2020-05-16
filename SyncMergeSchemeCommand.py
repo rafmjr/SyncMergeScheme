adf8706bca625d933b3419c27e0581b9cfadff1d
Rafael Mejia <rafamr.7@gmail.com>
2020-05-16 12:27:08 -0400

    Added detection of light and dark color schemes:
    Implemented basic detection of light and dark color schemes by searching
    for such words in the name of the applied scheme.
    
    Saving the Sublime Merge preferences file also removes the need for
    restarting the application in order to apply the changes.

--- SyncMergeSchemeCommand.py
+++ SyncMergeSchemeCommand.py
@@ -1,5 +1,6 @@
-import os, sublime, sublime_plugin
+import os, re, sublime, sublime_plugin
 from .packages.appdirs import user_data_dir
+from .packages.debounce import debounce
 
 class SyncMergeSchemeCommand(sublime_plugin.ApplicationCommand):
     def __init__(self):
@@ -13,9 +14,13 @@ class SyncMergeSchemeCommand(sublime_plugin.ApplicationCommand):
             'File Mode - Merge Dark.sublime-settings',
         ]
 
+    @debounce(1)
     def run(self):
         st_color_scheme = sublime.load_settings('Preferences.sublime-settings').get('color_scheme')
+        self.sync_color_scheme(st_color_scheme)
+        self.update_sm_preferences(st_color_scheme)
 
+    def sync_color_scheme(self, color_scheme):
         for file_name in self.sm_settings_files:
             file_path = os.path.join(self.sm_package_dir, file_name)
 
@@ -25,7 +30,21 @@ class SyncMergeSchemeCommand(sublime_plugin.ApplicationCommand):
             except:
                 sm_current_settings = {}
             finally:
-                sm_current_settings['color_scheme'] = st_color_scheme
+                sm_current_settings['color_scheme'] = color_scheme
 
             with open(file_path, 'w') as file:
                 file.write(sublime.encode_value(sm_current_settings, pretty = True))
+
+    def update_sm_preferences(self, color_scheme):
+        sm_preferences_path = os.path.join(self.sm_package_dir, 'Preferences.sublime-settings')
+
+        with open(sm_preferences_path, 'r') as file:
+            sm_preferences = sublime.decode_value(file.read())
+
+            if re.search('light', color_scheme):
+                sm_preferences['theme'] = 'Merge.sublime-theme'
+            elif re.search('dark', color_scheme):
+                sm_preferences['theme'] = 'Merge Dark.sublime-theme'
+
+        with open(sm_preferences_path, 'w') as file:
+            file.write(sublime.encode_value(sm_preferences, pretty = True))
